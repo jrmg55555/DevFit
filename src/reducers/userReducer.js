32fd7b34f39144b1cbb0e8cdc0923d73c15f6027
Jean Gabriel <jeanrodrigo30@gmail.com>
2020-01-08 17:12:39 -0400

    primeiro commit

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unreachable */
+const initialState = {
+  name: '',
+  level: '', //beginner, intermediate, advanced
+  workoutDays: [], // 1-0
+  myWorkouts: [],
+  lastWorkout: '', //ID
+  dailyProgress: ['2019-09-13', '2019-09-12'],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_NAME':
+      return {...state, name: action.payload.name};
+      break;
+  }
+  return state;
+};
