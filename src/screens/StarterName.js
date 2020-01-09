42b9d60f29471267f85450c4d9769e7943b48f78
Jean Gabriel <jeanrodrigo30@gmail.com>
2020-01-09 16:50:53 -0400

    #7 DevFit: StarterName

---
+++ src/screens/StarterName.js
@@ -0,0 +1,92 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-alert */
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {Button} from 'react-native';
+import {connect} from 'react-redux';
+import styled from 'styled-components/native';
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  align-items: center;
+  background-color: #fff;
+`;
+
+const HeaderText = styled.Text`
+  font-size: 22px;
+  color: #333;
+  margin: 50px 0;
+`;
+const NameInput = styled.TextInput`
+  border: 1px solid #ccc;
+  width: 100%;
+  height: 50px;
+  border-radius: 10px;
+  font-size: 16px;
+  padding: 10px;
+`;
+
+const NextButton = props => {
+  const nextAction = () => {
+    if (!props.navigation.state.params || !props.navigation.state.params.name) {
+      alert('Você precisa de um nome!');
+      return;
+    }
+    props.navigation.navigate('StarterDias');
+  };
+
+  return <Button title="Próximo" onPress={nextAction} />;
+};
+
+const Page = props => {
+  const nextAction = () => {
+    if (!props.name) {
+      alert('Você precisa de um nome!');
+      return;
+    }
+    props.navigation.navigate('StarterDias');
+  };
+
+  const changeTextName = t => {
+    props.setName(t);
+    props.navigation.setParams({name: t});
+  };
+
+  return (
+    <Container>
+      <HeaderText>Qual é o seu nome?</HeaderText>
+      <NameInput
+        value={props.name}
+        onChangeText={changeTextName}
+        autoFocus={true}
+        autoCapitalize="words"
+        onSubmitEditing={nextAction}
+      />
+    </Container>
+  );
+};
+
+Page.navigationOptions = ({navigation}) => {
+  return {
+    title: '',
+    headerRight: <NextButton navigation={navigation} />,
+    headerRightContainerStyle: {
+      marginRight: 10,
+    },
+  };
+};
+
+const mapStateToProps = state => {
+  return {
+    name: state.userReducer.name,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setName: name => setName(name, dispatch),
+    reset: () => reset(dispatch),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Page);
