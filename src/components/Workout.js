ee06454eccab662716632f39661fcabb21cfdce8
Jean Gabriel <jeanrodrigo30@gmail.com>
2020-01-14 14:06:52 -0400

    #13 DevFit: WorkoutComponent (2/2)

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,6 +1,8 @@
-/* eslint-disable react/self-closing-comp */
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import styled from 'styled-components/native';
+import useMuscleImage from '../components/useMuscleImage';
 
 const Workout = styled.View`
   background-color: #f1f1f1;
@@ -16,22 +18,61 @@ const WorkoutTitle = styled.Text`
   font-size: 17px;
   margin: 10px;
 `;
-const MuscleScroll = styled.ScrollView``;
-const WorkoutActions = styled.View``;
-const WorkoutButton = styled.View``;
-const WorkoutButtonImage = styled.Image``;
+const MuscleScroll = styled.ScrollView`
+  margin: 10px;
+`;
+const MuscleGroups = styled.View`
+  width: 40px;
+  height: 40px;
+  background-color: #ffcc98;
+  border-raius: 5px;
+  margin-right: 5px;
+  justify-content: center;
+  align-items: Center;
+`;
+const MuscleImage = styled.Image`
+  width: 30px;
+  height: 30px;
+`;
+const WorkoutActions = styled.View`
+  justify-content: center;
+`;
+const WorkoutButton = styled.View`
+  width: 25px;
+  height: 25px;
+  margin: 20px;
+  justify-content: Center;
+  align-items: center;
+`;
+const WorkoutButtonImage = styled.Image`
+  width: 25px;
+  height: 25px;
+`;
 
 export default props => {
+  let muscleGroups = [];
+  for (let i in props.data.exercices) {
+    if (!muscleGroups.includes(props.dataexercices[i].muscle)) {
+      muscleGroups.push(props.data.exercices[i].muscle);
+    }
+  }
+
   return (
     <Workout>
       <WorkoutInfo>
         <WorkoutTitle>{props.data.name}</WorkoutTitle>
-        <MuscleScroll horizontal={true}> </MuscleScroll>
+        <MuscleScroll horizontal={true}>
+          {muscleGroups.map((m, index) => (
+            <muscleGroups key={index}>
+              <SourceImage source={useMuscleImage(m)} />
+            </muscleGroups>
+          ))}
+        </MuscleScroll>
       </WorkoutInfo>
 
       <WorkoutActions>
         <WorkoutButton>
-          <WorkoutButtonImage />
+          <WorkoutButtonImage source={require('../assets/add.png')} />
         </WorkoutButton>
       </WorkoutActions>
     </Workout>
